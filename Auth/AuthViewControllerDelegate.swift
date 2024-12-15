b69f33938d21b099bb14ec845c9a304f5f9c7db8
danlncewerly <146477252+danlncewerly@users.noreply.github.com>
2024-12-15 17:22:38 +0300

    Add files via upload

---
+++ Auth/AuthViewControllerDelegate.swift
@@ -0,0 +1,4 @@
+import Foundation
+protocol AuthViewControllerDelegate: AnyObject {
+    func didAuthenticate(_ vc: AuthViewController)
+}
---
+++ Auth/Constants.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "SUtN67FyAT2ynN3GtRGqyeu3CshYdqbqJo7wQRnUUh0"
+    static let secretKey = "AdEc0nQ7eSn9D4Fj0LHuQDXlqTwiStpBaJkPnimf3LE"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL (string: "https://api.unsplash.com/")
+    static let AuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    static let defaultURL = URL(string: "https://unsplash.com/")
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    static let reuseIdentifier = "ImagesListCell"
+
+
+}
